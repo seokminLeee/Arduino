54545abe07c8955b3e0e7754704cbec73e194409
seokminLeee <125535138+seokminLeee@users.noreply.github.com>
2023-03-14 15:45:28 +0900

    Add files via upload

---
+++ 20230314/Sonar1/Sonar1.ino
@@ -0,0 +1,30 @@
+#define TRIG1 3 //초음파 센서1번 Trig핀번호
+#define ECHO1 4 //초음파 센서1번 ECHO핀번호
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+
+  Serial.begin(115200); // 통신속도를 115200으로 정의함
+}
+long sonar1(void) // 초음파 센서 1번 측정 함수
+{
+ long duration, distance;
+  digitalWrite(TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG1, LOW);
+  duration = pulseIn(ECHO1, HIGH);
+  distance = ( (float) (340*duration)/1000)/2;
+}
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  
+ 
+  //Serial.print("Duration: ");
+  //Serial.println(duration);
+  Serial.print("Distance: ");
+  Serial.println( sonar1() );
+  delay(500); 
+}
